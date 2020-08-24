63e3c361bff2844162fb821d3abe3fb76cd4995e
Umesh Dhaked <umdk456@gmail.com>
2020-08-24 19:53:41 +0000

    isUserAuthenticated middleware added

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import UserController from './../controller/UserController';
-var passport = require('passport');
 
 const router = express.Router();
 const uc = new UserController();
-const path = '/user';
+const path = '/user_detail';
 
-router.get(path + '/all', async (req, res) => {
+router.get(path + '/allUser', async (req, res) => {
+    console.log(req.session);
     await uc.getAllUsers()
         .then(result => {
             if (result) res.status(200).send(result)
@@ -30,12 +30,6 @@ router.get(path + '/getUserById/:id', async (req, res) => {
         })
 });
 
-router.post(path + '/save', async (req, res) => {
-    await uc.saveUser(req.body)
-        .then(result => { res.status(200).send(result) })
-        .catch(error => { res.status(500).send('some error occured') })
-});
-
 router.put(path + '/update', async (req, res) => {
 
     await uc.getAllUsers()
@@ -55,25 +49,6 @@ router.get(path + '/activate/:id', async (req, res) => {
     await uc.activateUserById(id)
         .then(result => { res.status(200).send(result) })
         .catch(error => { res.status(500).send(error) })
-});
-
-
-// This Funtions gets JsonData in this format -> 
-// {	
-//     "username":"abc@xyz.com",  
-//     "password":"1234"
-// }
-// Currently accepts login using 'email'.. so username value should be emailId.
-router.post(path + '/validate', (req, res, next) => {
-    passport.authenticate('local', function (err: any, user: any, info: any) {
-        if (err) { return next(err); }
-        if (!user) { return res.status(500).send('UserName or password is incorrect'); }
-
-        req.logIn(user, function (err) { 
-            if (err) { return next(err); }
-            return res.status(200).send("User SucessFully Authenticated");
-        });
-    })(req, res, next);
 });
 
 export default router;
